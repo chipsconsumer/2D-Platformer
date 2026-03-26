796521aefec20680c3f658731c18cfc5459f88e1
Chips consumer <youla.dc@gmail.com>
2026-03-26 09:44:11 +0100

    PlayerJump

--- Assets/PlayerMotor.cs
+++ Assets/PlayerMotor.cs
@@ -6,18 +6,48 @@ public class PlayerMotor : MonoBehaviour
     Vector2 direction;
     private bool canJump = true;
     private Rigidbody2D rigidbody2D;
-    public float speed = 10;
-    public float jumpforce = 10; 
-    //stworzyæ now¹ zmienna o nazwie jumpforce
+    public float speed = 5;
+    public float jumpforce = 5;
+    public float maxspeed = 10;
+    public float stoppingForce = 10;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
 rigidbody2D = GetComponent<Rigidbody2D>();
     }
     // Update is called once per frame
-    private void Update()
+    private void FixedUpdate()
     {
-        transform.position += new Vector3(direction.x, direction.y, 0) * Time.deltaTime * speed;
+        PlayerMovement();
+        HandleMaxSpeed();
+        PlayerStopping();
+
+    }
+
+    private void PlayerStopping()
+    {
+        if (direction.x == 0 && rigidbody2D.linearVelocityX != 0)
+
+        {
+            rigidbody2D.AddForce(new Vector2(-rigidbody2D.linearVelocityX * stoppingForce, 0));
+        }
+    }
+
+    private void HandleMaxSpeed()
+    {
+        if (rigidbody2D.linearVelocityX >= maxspeed)
+        {
+            rigidbody2D.linearVelocityX = maxspeed;
+        }
+        else if (rigidbody2D.linearVelocityX <= -maxspeed)
+        {
+            rigidbody2D.linearVelocityX = -maxspeed;
+        }
+    }
+
+    private void PlayerMovement()
+    {
+        rigidbody2D.AddForce(new Vector2(direction.x * speed, 0));
     }
 
     private void OnMove(InputValue value)
